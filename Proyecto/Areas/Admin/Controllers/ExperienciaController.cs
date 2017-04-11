ce30791f132f6ba509a6b5d113e12d5b5e72c5a8
gabriel <gabriel.chavez.r@gmail.com>
2017-04-11 07:47:48 -0400

    crud experiencia

--- Proyecto/Areas/Admin/Controllers/ExperienciaController.cs
+++ Proyecto/Areas/Admin/Controllers/ExperienciaController.cs
@@ -1,4 +1,6 @@
-﻿using Proyecto.Areas.Admin.Filters;
+﻿using Helper;
+using Proyecto.Areas.Admin.Filters;
+using Proyecto.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +12,49 @@ namespace proyecto.Areas.Admin.Controllers
     [Autenticado]
     public class ExperienciaController : Controller
     {
+        private Experiencia experiencia = new Experiencia();
         // GET: Admin/Experiencia
         public ActionResult Index(int tipo)
         {
+            ViewBag.tipo = tipo;
             ViewBag.Title = tipo == 1 ? "Trabajos realizados" : "Estudios previos";
             return View();
+        }
+        public ActionResult Crud(byte tipo, int id = 0)
+        {
+            if(id==0)
+            {
+                experiencia.Tipo = tipo;
+                experiencia.Usuario_id = SessionHelper.GetUser();
+            }
+            else
+            {
+                experiencia = experiencia.Obtener(id);
+            }
+
+            return View(experiencia);
+        }
+        public JsonResult Guardar(Experiencia model)
+        {
+            var rm = new ResponseModel();
+            if(ModelState.IsValid)
+            {
+                rm = model.Guardar();
+                if(rm.response)
+                {
+                    rm.href = Url.Content("~/admin/experiencia?tipo=" + model.Tipo);
+                }
+            }
+            return Json(rm);
+        }
+        public JsonResult Eliminar(int id)
+        {           
+            var rm = experiencia.Eliminar(id);
+            if (rm.response)
+            {
+                rm.href = Url.Content("self");
+            }
+            return Json(rm);
         }
     }
 }
--- Proyecto/Models/Experiencia.cs
+++ Proyecto/Models/Experiencia.cs
@@ -4,7 +4,9 @@ namespace Proyecto.Models
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity;
     using System.Data.Entity.Spatial;
+    using System.Linq;
 
     [Table("Experiencia")]
     public partial class Experiencia
@@ -35,5 +37,64 @@ namespace Proyecto.Models
         public string Descripcion { get; set; }
 
         public virtual Usuario Usuario { get; set; }
+
+        public Experiencia Obtener(int id)
+        {
+            var experiencia = new Experiencia();
+            try
+            {
+                using (var ctx = new ProyectoContext())
+                {
+                    experiencia = ctx.Experiencia.Where(x => x.id == id)
+                                .SingleOrDefault();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return experiencia;
+        }
+        public ResponseModel Guardar()
+        {
+            var rm = new ResponseModel();
+            try
+            {
+                using (var ctx = new ProyectoContext())
+                {
+                    if (this.id > 0) ctx.Entry(this).State = EntityState.Modified;
+                    else ctx.Entry(this).State = EntityState.Added;
+                    ctx.SaveChanges();
+                    rm.SetResponse(true);
+                }
+            }           
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return rm;
+        }
+        public ResponseModel Eliminar(int id)
+        {
+            var rm = new ResponseModel();
+            try
+            {
+                using (var ctx = new ProyectoContext())
+                {
+                    this.id = id;
+                    ctx.Entry(this).State = EntityState.Deleted;
+                    ctx.SaveChanges();
+                    rm.SetResponse(true);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return rm;
+        }
     }
 }
